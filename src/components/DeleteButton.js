ba198c784e49652c181e33f7203062acb4501137
sys-bbt <systems@brightbraintech.com>
2025-01-09 13:40:56 +0530

    Update DeleteButton.js

--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -6,7 +6,7 @@ const DeleteButton = ({ deliveryCode, onDelete }) => {
   const handleDelete = async () => {
     if (window.confirm(`Are you sure you want to delete delivery: ${deliveryCode}?`)) {
       try {
-        const response = await fetch(`http://localhost:3001/api/data/${encodeURIComponent(deliveryCode)}`, {
+        const response = await fetch(`https://server-ui-2.onrender.com/api/data/${encodeURIComponent(deliveryCode)}`, {
           method: 'DELETE',
           headers: {
             'Content-Type': 'application/json',
