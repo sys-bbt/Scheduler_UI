77a1e2749249892b04c068200e8a7127359d22ce
sys-bbt <systems@brightbraintech.com>
2025-06-10 12:31:50 +0530

    Add files via upload
    remove Duplicate lines

--- src/components/FilterDeliveryBasedOnClientSelected.js
+++ src/components/FilterDeliveryBasedOnClientSelected.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import { Dropdown } from 'react-bootstrap';
 
@@ -23,29 +22,3 @@ const FilterDeliveryBasedOnClientSelected = ({ clients, onClientSelect, selected
 };
 
 export default FilterDeliveryBasedOnClientSelected;
-=======
-import React from 'react';
-import { Dropdown } from 'react-bootstrap';
-
-const FilterDeliveryBasedOnClientSelected = ({ clients, onClientSelect, selectedClient }) => {
-  return (
-    <div>
-      <Dropdown>
-        <Dropdown.Toggle variant="success" id="dropdown-client">
-          {selectedClient || 'Filter by Client'}
-        </Dropdown.Toggle>
-        <Dropdown.Menu>
-          <Dropdown.Item onClick={() => onClientSelect('')}>All Clients</Dropdown.Item>
-          {clients.map((client, index) => (
-            <Dropdown.Item key={index} onClick={() => onClientSelect(client)}>
-              {client}
-            </Dropdown.Item>
-          ))}
-        </Dropdown.Menu>
-      </Dropdown>
-    </div>
-  );
-};
-
-export default FilterDeliveryBasedOnClientSelected;
->>>>>>> 887a083c3cfa83675dbbd621d281faaaef12030a
--- src/components/SortDeliveriesByDate.js
+++ src/components/SortDeliveriesByDate.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import { Form } from 'react-bootstrap';
 
@@ -19,25 +18,3 @@ const SortDeliveriesByDate = ({ sortOption, setSortOption }) => {
 };
 
 export default SortDeliveriesByDate;
-=======
-import React from 'react';
-import { Form } from 'react-bootstrap';
-
-const SortDeliveriesByDate = ({ sortOption, setSortOption }) => {
-  return (
-    <Form.Group controlId="sortDeliveriesByDate" className="mb-3">
-      <Form.Label>Sort by Date</Form.Label>
-      <Form.Control
-        as="select"
-        value={sortOption}
-        onChange={(e) => setSortOption(e.target.value)}
-      >
-        <option value="earliest">Earliest Initiated</option>
-        <option value="latest">Latest Initiated</option>
-      </Form.Control>
-    </Form.Group>
-  );
-};
-
-export default SortDeliveriesByDate;
->>>>>>> 887a083c3cfa83675dbbd621d281faaaef12030a
