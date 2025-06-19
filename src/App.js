c0f44267e9c642214779a6c72a27253e964efef2
sys-bbt <systems@brightbraintech.com>
2025-06-19 14:22:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { GoogleOAuthProvider } from '@react-oauth/google'; // Import the provider
+import React, { useContext } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import DeliveryList from './components/DeliveryList';
-import { UserProvider } from './components/UserContext'; // Import UserProvider
+import { UserProvider, UserContext, LoginComponent } from './components/UserContext';
 import DeliveryDetail from './components/DeliveryDetail';
+
+// New component to wrap authenticated routes
+// This component consumes the UserContext
+const AuthenticatedRoutes = () => {
+  const { userEmail } = useContext(UserContext); // Access userEmail within UserProvider's scope
+
+  // Debugging log for userEmail within AuthenticatedRoutes
+  console.log('AuthenticatedRoutes: userEmail from Context:', userEmail);
+
+  if (!userEmail) {
+    // If userEmail is null (not logged in), redirect to login page
+    console.log('AuthenticatedRoutes: User not logged in, redirecting to /login');
+    return <Navigate to="/login" replace />;
+  }
+
+  // If userEmail is available, render the protected routes
+  return (
+    <Routes>
+      <Route path="/" element={<DeliveryList />} />
+      <Route path="/delivery/*" element={<DeliveryDetail />} />
+      {/* Add other protected routes here if needed */}
+    </Routes>
+  );
+};
 
 function App() {
   return (
-    <GoogleOAuthProvider clientId='47439091557-2kdm5q9pi6lm0d2n3pvtvb6vr9p69h4h.apps.googleusercontent.com'> 
-      {/* Wrap your application with the provider */}
-      <UserProvider> {/* Wrap your app with UserProvider */}
+    <GoogleOAuthProvider clientId='47439091557-2kdm5q9pi6lm0d2n3pvtvb6vr9p69h4h.apps.googleusercontent.com'>
+      {/* UserProvider wraps the Router to make user context available everywhere */}
+      <UserProvider>
         <Router>
           <Routes>
-            <Route path="/" element={<DeliveryList />} />
-            <Route path="/delivery/*" element={<DeliveryDetail />} />
+            {/* Public route for login */}
+            <Route path="/login" element={<LoginComponent />} />
+            {/* All other routes are wrapped by AuthenticatedRoutes, which handles protection */}
+            <Route path="/*" element={<AuthenticatedRoutes />} />
           </Routes>
         </Router>
       </UserProvider>
