1bbddde4af4f8dd7d44672dfc1cd4dd8ca860116
sys-bbt <systems@brightbraintech.com>
2025-06-19 12:07:15 +0530

    Delete api/index.js

--- api/index.js
+++
@@ -1,6 +0,0 @@
-// api/index.js
-// This file acts as the primary serverless function entry point for your Express app.
-const app = require('./main-api'); // Import your main Express app
-
-// The 'handler' function for Vercel. This makes your Express app compatible.
-module.exports = app;
