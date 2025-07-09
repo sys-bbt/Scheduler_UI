a268a671ebf11792e30fad727447ed823c41d033
sys-bbt <systems@brightbraintech.com>
2025-07-09 16:55:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,43 +8,44 @@ import DeliveryDetail from './components/DeliveryDetail';
 // New component to wrap authenticated routes
 // This component consumes the UserContext
 const AuthenticatedRoutes = () => {
-  const { userEmail } = useContext(UserContext); // Access userEmail within UserProvider's scope
+    const { userEmail } = useContext(UserContext); // Access userEmail within UserProvider's scope
 
-  // Debugging log for userEmail within AuthenticatedRoutes
-  console.log('AuthenticatedRoutes: userEmail from Context:', userEmail);
+    // Debugging log for userEmail within AuthenticatedRoutes
+    console.log('AuthenticatedRoutes: userEmail from Context:', userEmail);
 
-  if (!userEmail) {
-    // If userEmail is null (not logged in), redirect to login page
-    console.log('AuthenticatedRoutes: User not logged in, redirecting to /login');
-    return <Navigate to="/login" replace />;
-  }
+    if (!userEmail) {
+        // If userEmail is null (not logged in), redirect to login page
+        console.log('AuthenticatedRoutes: User not logged in, redirecting to /login');
+        return <Navigate to="/login" replace />;
+    }
 
-  // If userEmail is available, render the protected routes
-  return (
-    <Routes>
-      <Route path="/" element={<DeliveryList />} />
-      <Route path="/delivery/*" element={<DeliveryDetail />} />
-      {/* Add other protected routes here if needed */}
-    </Routes>
-  );
+    // If userEmail is available, render the protected routes
+    return (
+        <Routes>
+            <Route path="/" element={<DeliveryList />} />
+            {/* UPDATED ROUTE: This now correctly captures the delCode */}
+            <Route path="/delivery/data/:delCode" element={<DeliveryDetail />} />
+            {/* Add other protected routes here if needed */}
+        </Routes>
+    );
 };
 
 function App() {
-  return (
-    <GoogleOAuthProvider clientId='47439091557-2kdm5q9pi6lm0d2n3pvtvb6vr9p69h4h.apps.googleusercontent.com'>
-      {/* UserProvider wraps the Router to make user context available everywhere */}
-      <UserProvider>
-        <Router>
-          <Routes>
-            {/* Public route for login */}
-            <Route path="/login" element={<LoginComponent />} />
-            {/* All other routes are wrapped by AuthenticatedRoutes, which handles protection */}
-            <Route path="/*" element={<AuthenticatedRoutes />} />
-          </Routes>
-        </Router>
-      </UserProvider>
-    </GoogleOAuthProvider>
-  );
+    return (
+        <GoogleOAuthProvider clientId='47439091557-2kdm5q9pi6lm0d2n3pvtvb6vr9p69h4h.apps.googleusercontent.com'>
+            {/* UserProvider wraps the Router to make user context available everywhere */}
+            <UserProvider>
+                <Router>
+                    <Routes>
+                        {/* Public route for login */}
+                        <Route path="/login" element={<LoginComponent />} />
+                        {/* All other routes are wrapped by AuthenticatedRoutes, which handles protection */}
+                        <Route path="/*" element={<AuthenticatedRoutes />} />
+                    </Routes>
+                </Router>
+            </UserProvider>
+        </GoogleOAuthProvider>
+    );
 }
 
 export default App;
