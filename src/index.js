4078114a53d98c9f0e8de800036ca50511baa5bb
sys-bbt <systems@brightbraintech.com>
2025-07-11 14:28:08 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 // src/index.js (or src/main.jsx)
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // For React 18+
+import ReactDOM from 'react-dom/client';
 import App from './App';
-import { UserProvider } from './components/UserContext'; // Ensure this path is correct
-// import 'bootstrap/dist/css/bootstrap.min.css'; // If you're using Bootstrap
+import { UserProvider } from './components/UserContext';
 
-// Get the root DOM element where your React app will be mounted
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
-        {/* UserProvider must wrap the entire App to make UserContext available everywhere */}
-        <UserProvider>
+        <UserProvider> {/* This is crucial */}
             <App />
         </UserProvider>
     </React.StrictMode>
