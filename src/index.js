79bc6d11c6172d99a20dd9f272615676d67b0272
sys-bbt <systems@brightbraintech.com>
2025-07-11 11:25:55 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
+// index.js or main.jsx
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom/client'; // For React 18+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { UserProvider } from './components/UserContext'; // Import UserProvider
 
-
+// Ensure your root element exists, e.g., <div id="root"></div> in index.html
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+    <React.StrictMode>
+        <UserProvider> {/* Wrap your entire App with UserProvider */}
+            <App />
+        </UserProvider>
+    </React.StrictMode>
+);
