9ee2be13882b88c38c49c425c1d27346a75742de
sys-bbt <systems@brightbraintech.com>
2025-11-12 17:37:25 +0530

    Update UserContext.js

--- src/components/UserContext.js
+++ src/components/UserContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useEffect } from 'react';
+import React, { createContext, useState } from 'react';
 import { GoogleLogin } from '@react-oauth/google';
 import { jwtDecode } from 'jwt-decode'; // Make sure 'jwt-decode' is installed
 
