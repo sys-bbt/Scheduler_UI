1aad6b7f920ed6e719b139eb8f76f83fc5678846
sys-bbt <systems@brightbraintech.com>
2025-11-13 10:29:39 +0530

    Update FilterDeliveryBasedOnClientSelected.js

--- src/components/FilterDeliveryBasedOnClientSelected.js
+++ src/components/FilterDeliveryBasedOnClientSelected.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Dropdown } from 'react-bootstrap';
 
-// FIX: Renamed props to match DeliveryList.js and added default empty array to allClients to prevent runtime error
-const FilterDeliveryBasedOnClientSelected = ({ allClients = [], handleClientSelect, selectedClient }) => {
+const FilterDeliveryBasedOnClientSelected = ({ clients = [], onClientSelect, selectedClient }) => {
   return (
     <div>
       <Dropdown>
@@ -10,11 +9,10 @@ const FilterDeliveryBasedOnClientSelected = ({ allClients = [], handleClientSele
           {selectedClient || 'Filter by Client'}
         </Dropdown.Toggle>
         <Dropdown.Menu>
-          {/* FIX: Changed handler name to handleClientSelect */}
-          <Dropdown.Item onClick={() => handleClientSelect('')}>All Clients</Dropdown.Item>
-          {/* FIX: Now safely mapping over allClients */}
-          {allClients.map((client, index) => (
-            <Dropdown.Item key={index} onClick={() => handleClientSelect(client)}>
+          <Dropdown.Item onClick={() => onClientSelect('')}>All Clients</Dropdown.Item>
+          {clients.map((client, index) => (
+            // Ensure client is a string or has a value property before using as key
+            <Dropdown.Item key={client || index} onClick={() => onClientSelect(client)}>
               {client}
             </Dropdown.Item>
           ))}
